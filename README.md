dfa5057119514ac9607870cbb74d149c86f21ddb
Silvan Schermer <dev@silvanschermer.com>
2022-02-05 11:36:40 +0100

    ADDED: Explanations to Kernel::start on what will be added in future revisions

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ WIP Simple Framework with Routing, Request injection etc...
 
 ## SETUP
 1 . run Composer Update.
-1. point your htaccess, site config or x to /public.
+2. point your htaccess, site config or x to /public.
--- src/Kernel.php
+++ src/Kernel.php
@@ -20,6 +20,9 @@ class Kernel
     public static function start() : void
     {
         (new Configuration())->handleUserConfigugration();
+
+        // capture the request pars and modify then pass the cooked request to handlerequest which will reflect on the controller parameters look for some class of type ?baserequest? and  inject
+        // the captured request can be handled by e.g. a google captcha middleware... if the code is accepted we can continue and handle request as normal. At this point at max a generic request post or get object should exist from which a specific request with field validation can be cast upont it.
         (new Router())->handleRequest();
     }
 }
